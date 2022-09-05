7af1e0175afb36d37cc0c641548c4a0e5251a7e2
faizal-siddiqui <faizalsidp@gmail.com>
2022-09-06 00:42:35 +0530

    Name File

---
+++ DSA/unit-1/palindrome.js
@@ -0,0 +1,14 @@
+let str = "naman";
+let N = str.length;
+
+let new_str = "";
+for(let i=N-1; i>=0; i--){
+    new_str += str[i];
+}
+
+if(str === new_str){
+    console.log("Yes");
+} else{
+    console.log("No");
+}
+
